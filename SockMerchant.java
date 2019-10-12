59b441179010801769066fe79ca4316c802a0480
Marcio Costa <margeodev@gmail.com>
2019-10-12 13:42:22 -0300

    Interview Preparation kit 1/69

---
+++ SockMerchant.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SockMerchant {
+
+	static int sockMerchant(int n, int[] ar) {
+		Set<Integer> colors = new HashSet<>();
+	    int pairs = 0;
+
+	    for (int i = 0; i < n; i++) {
+	        if (!colors.contains(ar[i])) {
+	            colors.add(ar[i]);
+	        } else {
+	            pairs++;
+	            colors.remove(ar[i]);
+	        }
+	    }
+	    return pairs;
+    }
+	
+	public static void main(String[] args) {
+		int[] startValues = {10, 20, 20, 10, 10, 30, 50, 10, 20};
+		
+		sockMerchant(9, startValues);
+	}
+}
